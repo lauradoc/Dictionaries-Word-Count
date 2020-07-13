8ec6eee03ebcb9289329c11ac83393e6f5c34f1d
Laura Docherty <laurahdocherty@gmail.com>
2020-07-13 18:50:03 +0000

    Initial commit

---
+++ wordcount.py
@@ -0,0 +1,17 @@
+
+
+
+def word_count(text):
+    """Returns number of times a word appears in text"""
+
+    open_text = open("text.txt") 
+
+    word_count = {}
+
+    for word in open_text:
+        if word_count = word_count.get(word, 0) + 1
+
+    return word_count
+
+
+
