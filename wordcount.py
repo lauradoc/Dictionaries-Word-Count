91431b84a24fc7b114e676faed1972cf2f25ac0a
Laura Docherty <laurahdocherty@gmail.com>
2020-07-13 19:26:07 -0500

    Lab complete

--- wordcount.py
+++ wordcount.py
@@ -1,17 +1,28 @@
 
+import sys
 
+text = sys.argv[1]
+# def word_count(text):
+"""Returns number of times a word appears in text"""
 
-def word_count(text):
-    """Returns number of times a word appears in text"""
+open_text = open(text)
+word_count = {}  
+all_words = []  
 
-    open_text = open("text.txt") 
+for line in open_text:
+    line = line.split(' ')
+    punctuations = [',', '?', '.', '"', "'", ':']
+    for word in line:
+        for character in word:
+            if character in punctuations:
+                word = word.replace(character, '')
+        word = word.lower().strip()
+        all_words.append(word)
+        word_count[word] = word_count.get(word, 0) + 1
 
-    word_count = {}
+for key, value in word_count.items():
+        print(f'{key} {value}')
 
-    for word in open_text:
-        if word_count = word_count.get(word, 0) + 1
-
-    return word_count
 
 
 
